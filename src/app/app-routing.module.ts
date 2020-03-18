9eb5e3136fe270cf0b0bebd8cfefdf0530fc8686
Nataliya Kokovina <kokovina.nat>
2020-03-18 18:56:51 +0200

    authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { CreateProfileComponent } from '@views/create-profile/create-profile.component';
+import { AuthGuard } from '@services/auth.guard'
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: CreateProfileComponent}
+  { path: 'profile', component: CreateProfileComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
