1fd4172d523f21a41a129fcc07354b8c59e971b8
andreyzagoruy-dev <andreyzagoruy.dev@gmail.com>
2020-05-02 13:06:47 +0300

    Places autosuggestion remake. TODO: Look for dynamic country solution

--- src/app/components/places-suggest/places-suggest.component.ts
+++ src/app/components/places-suggest/places-suggest.component.ts
@@ -10,8 +10,6 @@ import { debounceTime, filter } from 'rxjs/operators';
 })
 export class PlacesSuggestComponent implements OnInit {
   public address = false;
-  public code = '0,0';
-
   public searchQuery = new FormControl('');
 
   public suggestedPlaces = [];
@@ -20,33 +18,29 @@ export class PlacesSuggestComponent implements OnInit {
 
   constructor(private placesGeocode: PlacesGeocodeService) { }
 
- ngOnInit() {
-    this.placesGeocode.getPosition().then( pos =>{
-      this.code =`${pos.lat},${pos.lng}`;
-    });
-
+  ngOnInit() {
     this.searchQuery.valueChanges
       .pipe(
         filter((place) => place.length),
         debounceTime(1500)
       )
       .subscribe((address) => {
-        this.placesGeocode.suggest(address, this.code).subscribe((places: any) => {
+        this.placesGeocode.suggest(address).subscribe((places: any) => {
           this.address = false;
           this.suggestedPlaces = places.items;
-
         });
       });
   }
 
   setPlace(place: any) {
-    if(place.address.houseNumber){
-      this.searchQuery.setValue(`${place.address.city}, ${place.address.street}, ${place.address.houseNumber}`);
+    if (place.address.houseNumber) {
+      this.searchQuery.setValue(`${place.address.street}, ${place.address.houseNumber}, ${place.address.city}`, {
+        emitEvent: false
+      });
       this.addLocation.emit(place.position);
       this.address = true;
     } else {
       this.searchQuery.setValue(`${place.address.city}, ${place.address.street},`);
     }
-
   }
 }
