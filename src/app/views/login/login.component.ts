00399e712e1c76b7290dfc642fc70005e15fe96f
andreyzagoruy-dev <andreyzagoruy.dev@gmail.com>
2020-05-22 14:46:31 +0300

    Active trip fetching on login added

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { AuthService } from '@services/auth.service';
+import { ApiService } from '@app/services/api.service';
 import { StorageService } from '@services/storage.service';
 
 @Component({
@@ -15,13 +17,21 @@ export class LoginComponent {
   constructor(
     private router: Router,
     private auth: AuthService,
+    private api: ApiService,
     private storage: StorageService
   ) { }
 
   login() {
     this.auth.login(this.email, this.password)
-      .subscribe((userFromServer) => {
-        this.storage.setItem('userProfile', userFromServer);
+      .pipe(
+        switchMap(
+          ({ id }) => this.api.getActiveTrip(id),
+          (userProfile, activeTrip) => ({ userProfile, activeTrip })
+        )
+      )
+      .subscribe(({ userProfile, activeTrip }) => {
+        this.storage.setItem('userProfile', userProfile);
+        this.storage.setItem('activeTrip', activeTrip);
         this.router.navigate(['/trips']);
       });
   }
