cc6c243e511dd7b34b714b2735eee60802c0f268
andreyzagoruy-dev <andreyzagoruy.dev@gmail.com>
2020-05-23 19:06:57 +0300

    API route for prod environment changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '//localhost:3000/api',
+  apiBaseUrl: '/api',
   apiMapsUrl: 'https://geocode.search.hereapi.com/v1/geocode?apiKey=',
   apiMapsKey: 'PlBaCE42NHOrkIaoJA_XyUfT8ea0HIUaLpumgDgADrE'
 };
