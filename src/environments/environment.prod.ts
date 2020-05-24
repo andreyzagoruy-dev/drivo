356b6ab29c5e849cb5b448a1268e5f3bf51ae0a6
andreyzagoruy-dev <andreyzagoruy.dev@gmail.com>
2020-05-24 15:37:44 +0300

    Production API url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://give-a-ride.pp.ua:3000/api',
+  apiBaseUrl: '/api',
   apiMapsUrl: 'https://geocode.search.hereapi.com/v1/geocode?apiKey=',
   apiMapsKey: 'PlBaCE42NHOrkIaoJA_XyUfT8ea0HIUaLpumgDgADrE'
 };
