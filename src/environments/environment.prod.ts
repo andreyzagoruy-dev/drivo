1516d6c0ad31d57f8d45a15aee6f75014acb5985
Andrii Zahorui <andrii@Andriis-Air.lan>
2024-09-15 11:54:43 +0300

    Removed ignored files

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: '/api',
-  apiMapsUrl: 'https://geocode.search.hereapi.com/v1/geocode?apiKey=',
-  apiMapsKey: '-2ULcl4-S5xx9jyodjEaVNdr2g7oDPnkMG796xnTkoQ'
-};
